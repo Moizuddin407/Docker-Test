b2fee51c149d7ce4c9a3b14552d4089c52008865
Moiz Uddin M. Abdullah <moiz211j@gmail.com>
2025-02-12 05:17:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Backend API!');
+});
+
+// Example API route
+app.get('/api/data', (req, res) => {
+  res.json({
+    message: 'This is sample data from the API.',
+    data: [1, 2, 3, 4, 5]
+  });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
